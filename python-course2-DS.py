77ae1bce71f5aed737a38ee495d2198a37087c3b
anand-swaroop-git <prashu.swaroop@gmail.com>
2018-09-17 00:35:43 +0530

    Committing first changes

---
+++ python-course2-DS.py
@@ -0,0 +1,30 @@
+# String as array
+# fruit = 'banana'
+# x = fruit[0]
+# print(x)
+
+#String regex or computations in array
+# y = 3
+# fruit = 'abcdefg'
+# x = fruit[y - 1]
+# print(x)
+
+# Finding the length of the string using len function
+# fruit = 'banana'
+# x = print(len(fruit))
+
+# Looping through strings
+# userinput = input('Please enter the string: ')
+# try:
+#     test = float(userinput)
+#     print('Please enter a string and not a number!')
+# except:
+#     string = userinput
+#     index = 0
+#     length = len(userinput)
+#     while index < length:
+#         print(index, string[index])
+#         index = index + 1
+    
+
+
