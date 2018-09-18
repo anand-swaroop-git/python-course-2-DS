0a91acd370d8bad11d7262056b8fc6aeec36991f
payne <prashu.swaroop@email.com>
2018-09-18 19:33:59 +0530

    Completed all assignments of week 4

--- week-4-lists-n-strings.py
+++ week-4-lists-n-strings.py
@@ -160,7 +160,14 @@ t = [9, 41, 12, 3, 74, 15]
 # stuff.sort()
 # print(stuff)
 
-     
-
-
-    
+# Assignment 8.5 which needs mbox-short.txt
+# fname = input("Enter file name: ")
+# fh = open(fname)
+# count = 0
+# for line in fh:
+#     line = line.rstrip()
+#     if not line.startswith('From ') : continue
+#     words = line.split()
+#     count = count + 1
+#     print(words[1])
+# print("There were", count, "lines in the file with From as the first word")   
