0b2d8c27e615c2a20adf5e28361fe50ccb0db5f5
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-19 08:48:02 +0530

    Some commits

---
+++ dictionaries-week-5.py
@@ -0,0 +1,44 @@
+# Dictionaries
+>>>purse = dict()​
+>>>purse['money'] = 12​
+>>>purse['candy'] = 3​
+>>>purse['tissues'] = 75​
+>>>print(purse)​
+# {'money': 12, 'tissues': 75, 'candy': 3}​
+>>>print(purse['candy'])​
+# 3​
+>>>purse['candy'] = purse['candy'] + 2​
+>>>print(purse)​
+# {'money': 12, 'tissues': 75, 'candy': 5}
+
+# Comparing Lists and Dictionaries
+    # Lists
+    # >>> lst = list()​
+    # >>> lst.append(21)​
+    # >>> lst.append(183)​
+    # >>> print(lst)​
+    # [21, 183]​
+    # >>> lst[0] = 23​
+    # >>> print(lst)​
+    # [23, 183]​
+
+    # Dictionaries
+    # >>> ddd = dict()​
+    # >>> ddd['age'] = 21​
+    # >>> ddd['course'] = 182​
+    # >>> print(ddd)​
+    # {'course': 182, 'age': 21}​
+    # >>> ddd['age'] = 23​
+    # >>> print(ddd)​
+    # {'course': 182, 'age': 23}
+
+# Dictionary Literals (Constants)
+# Dictionary literals use curly braces and have a list of key : value pairs​
+# You can make an empty dictionary using empty curly braces​
+# >>> jjj = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}​
+# >>> print(jjj)​
+# {'jan': 100, 'chuck': 1, 'fred': 42}​
+# >>> ooo = { }​
+# >>> print(ooo)​
+# {}​
+# >>>​
