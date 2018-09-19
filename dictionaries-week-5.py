6bac10dc831c37e3eacdde6076139e4d6075af93
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-20 00:57:51 +0530

    few more again

--- dictionaries-week-5.py
+++ dictionaries-week-5.py
@@ -81,3 +81,27 @@
 # for name in names :​
 #     counts[name] = counts.get(name, 0) + 1​
 # print(counts)
+
+# Counting Pattern
+# The general pattern to count the words in a line of text is to split the line into words, then loop through the words and use a dictionary to track the count of each word independently.​
+# counts = dict()​
+# print('Enter a line of text:')​
+# line = input('')​
+# words = line.split()​
+# print('Words:', words)​
+# print('Counting...')​
+# for word in words:​
+#     counts[word] = counts.get(word,0) + 1​
+# print('Counts', counts)​
+
+# Definite Loops and Dictionaries​
+# Even though dictionaries are not stored in order, we can write a for loop that goes through all the entries in a dictionary - actually it goes through all of the keys in the dictionary and looks up the values​
+# >>> counts = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}​
+# >>> for key in counts:​
+# ...     print(key, counts[key])​
+# ... ​
+# jan 100​
+# chuck 1​
+# fred 42​
+# >>>
+
