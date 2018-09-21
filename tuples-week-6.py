72f3eb1fc43fc6bc8e12594b1c079bf609f7de14
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-21 08:29:07 +0530

    Submitted final assignment

--- tuples-week-6.py
+++ tuples-week-6.py
@@ -105,3 +105,21 @@
 # >>> c = {'a':10, 'b':1, 'c':22}
 # >>> print( sorted( [ (v,k) for k,v in c.items() ] ) )
 # [(1, 'b'), (10, 'a'), (22, 'c')]
+
+# Assignment 10.2 which needs mbox-short.txtname = raw_input("Enter file:")
+# if len(name) < 1 : name = "mbox-short.txt"
+# handle = open(name)
+# counts = dict()
+# for line in handle :
+# 	line = line.rstrip()
+# 	if line == "": continue
+# 	words = line.split()
+# 	if words[0] != "From" : continue
+# 	time = words[5].split(":")
+# 	counts[time[0]] = counts.get(time[0], 0) + 1
+# list = list()
+# for key,value in counts.items() :
+# 	list.append((key,value))
+# list.sort()
+# for hour,count in list :
+# 	print(hour, count)
